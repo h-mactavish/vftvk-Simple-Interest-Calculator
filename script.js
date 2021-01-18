2f134becefb2c70d15f386cf7de05c9d4650629b
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-18 06:27:07 +0530

    First commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var pricipal_amount = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var time = document.getElementById("years").value;
+
+  var simple_interest = (pricipal_amount * rate * time) / 100;
 }
-        
