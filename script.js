788db142513bdc18f43d50dd57167eb90692b448
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-18 06:38:47 +0530

    Added span output to range type

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function compute() {
   var pricipal_amount = document.getElementById("principal").value;
   var rate = document.getElementById("rate").value;
   var time = document.getElementById("years").value;
+  var range_val = document.getElementById("s_range");
+  range_val.innerHTML = rate.value;
 
   var simple_interest = (pricipal_amount * rate * time) / 100;
 }
