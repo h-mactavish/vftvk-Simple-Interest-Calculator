93b5a66b106db1a2ecc4e2f901a6ef9ea0090662
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-18 06:56:01 +0530

    added css and js

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function compute() {
   var pricipal_amount = document.getElementById("principal").value;
   var rate = document.getElementById("rate").value;
   var time = document.getElementById("years").value;
-  var range_val = document.getElementById("s_range");
-  range_val.innerHTML = rate.value;
+  var output = document.getElementById("s_range");
+  output.innerHTML = rate.value;
+  rate.oninput = function () {
+    output.innerHTML = this.value;
+  };
 
   var simple_interest = (pricipal_amount * rate * time) / 100;
 }
